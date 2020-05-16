7ef4425928e1ab390154628f448e8057ad9bc68b
haha0201 <47706454+haha0201@users.noreply.github.com>
2020-05-15 21:58:42 -0400

    ok boomer

--- main.js
+++ main.js
@@ -3891,29 +3891,37 @@ function update() {
           enemies.push(new IceEnemy(220, 39, 1.3, 115));
           enemies.push(new IceEnemy(240, 40, 1.1, 115));
         }else if (wave == 87){
-          enemies.push(new IceEnemy(180, 20, 3, 115));
-          enemies.push(new IceEnemy(180, 20, 3, 115));
+          enemies.push(new IceEnemy(180, 20, 1, 115));
+          enemies.push(new IceEnemy(180, 20, 1, 115));
           for (var i = 10; i--;){
-              enemies.push(new IcicleEnemy(60, 25, 3.5, 60));
+              enemies.push(new IcicleEnemy(60, 25, 3, 60));
           }
         }else if (wave == 88){
-          enemies.push(new IceEnemy(180, 20, 3, 115));
-          enemies.push(new IceEnemy(180, 20, 3, 115));
-          for (var i = 7; i--;){
+          enemies.push(new IceEnemy(180, 20, 2, 115));
+          enemies.push(new IceEnemy(180, 20, 2, 115));
+          for (var i = 12; i--;){
               enemies.push(new IcicleEnemy(60, 25, 3.5, 60));
-              enemies.push(new LiquidEnemy(60, 25, 1));
           }
         }else if (wave == 89){
-          enemies.push(new IceEnemy(180, 20, 3, 115));
-          enemies.push(new IceEnemy(180, 20, 3, 115));
-          for (var i = 7; i--;){
+          enemies.push(new IceEnemy(180, 20, 2.5, 115));
+          enemies.push(new IceEnemy(180, 20, 2.5, 115));
+          for (var i = 6; i--;){
               enemies.push(new IcicleEnemy(60, 25, 3.5, 60));
               enemies.push(new LiquidEnemy(60, 25, 1));
           }
-          enemies.push(new SlowerEnemy(200, 25, 1, 50));
-          enemies.push(new SlowerEnemy(200, 25, 1, 50));
+          enemies.push(new SlowerEnemy(200, 25, 1, 100));
+          enemies.push(new SlowerEnemy(200, 25, 1, 100));
+        }else if (wave == 90){
+          enemies.push(new IceEnemy(180, 20, 3, 115));
+          enemies.push(new IceEnemy(180, 20, 3, 115));
+          for (var i = 6; i--;){
+              enemies.push(new IcicleEnemy(60, 25, 3.5, 60));
+              enemies.push(new LiquidEnemy(60, 25, 1));
+          }
+          enemies.push(new SlowerEnemy(200, 25, 1, 100));
+          enemies.push(new SlowerEnemy(200, 25, 1, 100));
           enemies.push(new SniperEnemy(150, 25, 0.5, 70, 25, 6, 5));
-          enemies.push(new ExploderEnemy(150, 45, 0.5, 50));
+          enemies.push(new ExploderEnemy(150, 45, 0.5, 70));
         }
         else{
             
