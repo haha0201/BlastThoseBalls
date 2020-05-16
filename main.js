a33bd1d740878808d8b671c77aa0ccfeb296ef89
E12345EE12345E <60201680+E12345EE12345E@users.noreply.github.com>
2020-05-15 21:45:38 -0700

    i fixed it lol sorry im a noob

--- main.js
+++ main.js
@@ -3930,7 +3930,7 @@ function update() {
           enemies.push(new ShieldEnemy(100, 35, 1.2, 60, 60));
           }
           for (var i = 1; i--;){
-          enemies.push(new DelayShieldEnemy(100, 35, 1.2, 60, 60));
+          enemies.push(new DelayShieldEnemy(100, 35, 1.2, 60, 60, 30));
           }
         } else if (wave == 63){
           for (var i = 3; i--;){
